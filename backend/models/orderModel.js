12ecdbcdf849db52b9ce0626700902221b7068d8
tomdan-ai <tomudoh258@gmail.com>
2025-07-29 19:14:37 +0100

    Baki

---
+++ backend/models/orderModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const orderSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  phone: { type: String, required: true },
+  address: { type: String, required: true },
+  items: [
+    {
+      id: String,
+      name: String,
+      price: Number,
+      image: String,
+      quantity: Number,
+    }
+  ],
+  total: { type: Number, required: true },
+  status: { type: String, default: 'processing' },
+  date: { type: Date, default: Date.now },
+})
+
+module.exports = mongoose.model('Order', orderSchema)
